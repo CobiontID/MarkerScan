bb151a5b6ea33d23358b78bc922f57b007eaeae8
emvcaest <emmelien.vancaester@gmail.com>
2022-12-14 15:22:34 +0000

    Clean up and add comments

--- scripts/AddTaxIDKraken.py
+++ scripts/AddTaxIDKraken.py
@@ -6,12 +6,18 @@ import sys
 import glob
 import json
 
+'''
+This scripts takes as an input a data directory where the ncbi dataset is downloaded 
+and will reformat all underlying fasta files to include their taxid in each '>' header e.g. >contig1|kraken:taxid|9606
+'''
+
 parser = argparse.ArgumentParser()
 parser.add_argument("-d", type=str, action='store', dest='directory', metavar='DIR',help='define refseq directory')
 parser.add_argument("-o", type=str, action='store', dest='output', metavar='OUT',help='define output file')
 parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 results = parser.parse_args()
 
+## loop over assembly_data_report.jsonl file to have all taxids per asmid
 taxfile=results.directory +'/ncbi_dataset/data/assembly_data_report.jsonl'
 
 with open(taxfile, 'r') as json_file:
@@ -35,26 +41,17 @@ for json_str in json_list:
     SpeciesDictionary[acc2]=taxid
     print(acc+'\t'+str(taxid))        
 
+## loop over all fasta files and adapt their seq headers
 g=open(results.output,'w')
 files = glob.glob(results.directory + '/*/*/*/*.fna')
 for fastafile in files:
     print(fastafile)
-    #if os.path.isfile(os.path.join(os.path.dirname(fastafile),"data_report.yaml")):
-    #    taxfile=os.path.join(os.path.dirname(fastafile),"data_report.yaml")
-    #    print(fastafile+'\t'+taxfile)
     taxID=0    
-    #   l=open(taxfile,'r')
-    #   for record in l:
-    #        record=record.rstrip()
-    #        if 'tax_id' in record:
-    #            taxID=record.split(':')[1].strip()
-    #    l.close()
     f =open(fastafile,'r')
     for record in f:
         if record.startswith(">"):
             record=record.strip()
             accession=fastafile.split('/')[-2]
-            #print(accession)
             if accession in SpeciesDictionary:
                 newline=record.split(' ')[0]+"|kraken:taxid|"+str(SpeciesDictionary[accession])+" "+" ".join(record.split(' ')[1:])
             else:
@@ -65,13 +62,3 @@ for fastafile in files:
             g.write(record+"\n")
     f.close()
 g.close()
-
-#f =open(results.fasta,'r')
-#for record in f:
-#        if record.startswith(">"):
-#            record=record.strip()
-#            newline=record.split(' ')[0]+"|kraken:taxid|"+str(results.tax).strip()+" "+" ".join(record.split(' ')[1:])
-#            print(newline)
-#        else:
-#            record=record.strip()
-#            print(record)
